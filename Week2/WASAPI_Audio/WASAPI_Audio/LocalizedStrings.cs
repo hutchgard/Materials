84dd3411e91cf7dfd1bc6847aaff0450384a0a05
hutchgard <hutchgard1@hotmail.com>
2014-04-14 13:16:17 -0700

    New project created for hw solution

---
+++ Week2/WASAPI_Audio/WASAPI_Audio/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WASAPI_Audio.Resources;
+
+namespace WASAPI_Audio
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
