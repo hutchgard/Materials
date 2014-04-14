23d63f1654c32e6334048fdf76ecf3f3ef5696d8
hutchgard <hutchgard1@hotmail.com>
2014-04-14 11:47:35 -0700

    hw update

---
+++ Week2/Homework/WASAPI_Project/WASAPI_Project/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WASAPI_Project.Resources;
+
+namespace WASAPI_Project
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ Week2/Homework/WASAPI_Project/WASAPI_Project/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using WASAPI_Project.Resources;
+using WASAPI_Audio;
+
+namespace WASAPI_Project
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        
+        private WASAPI_Audio.WASAPI AudioLoop = new WASAPI_Audio.WASAPI();
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        //private void StartAudio_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        //{
+        //    AudioLoop.startThread();
+        //}
+
+        //private void StopAudio_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        //{
+        //    AudioLoop.stopThread();
+        //}
+
+       
+    }
+}
